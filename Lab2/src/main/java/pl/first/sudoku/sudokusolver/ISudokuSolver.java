0d3d04e5bb0a2f27137dc77300ebb89f1e132e5a
Daniyar Zhumatayev <253857@edu.p.lodz.pl>
2025-03-25 19:02:11 +0100

    Created a new interface file

---
+++ Lab2/src/main/java/pl/first/sudoku/sudokusolver/ISudokuSolver.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pl.first.sudoku.sudokusolver;
+
+/**
+ * Interface for Sudoku solving algorithms
+ * @author zhuma
+ */
+public interface ISudokuSolver {
+    boolean solve(SudokuBoard board);
+}
