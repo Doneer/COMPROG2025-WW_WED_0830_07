c585ef05399a7c9e830c3903aaa26e1d0d98d5fd
Daniyar Zhumatayev <253857@edu.p.lodz.pl>
2025-03-26 01:00:58 +0100

    Added BacktrackingSudokuSolverTest.java

---
+++ Lab2/src/test/java/pl/first/sudoku/sudokusolver/BacktrackingSudokuSolverTest.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package pl.first.sudoku.sudokusolver;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author zhuma
+ */
+public class BacktrackingSudokuSolverTest {
+    @Test
+    public void testSolve() {
+        ISudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard board = new SudokuBoard(solver);
+        
+        assertTrue(solver.solve(board), "Solving should be successful");
+        assertTrue(board.isValid(), "Board should be valid after solving");
+        
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                int value = board.getValueAt(row, col);
+                assertTrue(value >= 1 && value <= 9, 
+                        "All cells should have values between 1 and 9");
+            }
+        }
+    }
+
+    @Test
+    public void testSolveWithPrefilledValues() {
+        ISudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard board = new SudokuBoard(solver);
+
+        board.setValueAt(0, 0, 5);
+        board.setValueAt(1, 1, 3);
+        board.setValueAt(2, 2, 9);
+        
+        assertTrue(solver.solve(board), "Solving with pre-filled values should succeed");
+    }
+    
+    @Test
+    public void testRandomSolutions() {
+        ISudokuSolver solver1 = new BacktrackingSudokuSolver();
+        ISudokuSolver solver2 = new BacktrackingSudokuSolver();
+        
+        SudokuBoard board1 = new SudokuBoard(solver1);
+        SudokuBoard board2 = new SudokuBoard(solver2);
+        
+        assertTrue(solver1.solve(board1), "First board solving should succeed");
+        assertTrue(solver2.solve(board2), "Second board solving should succeed");
+
+        boolean different = false;
+        int differences = 0;
+        
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board1.getValueAt(row, col) != board2.getValueAt(row, col)) {
+                    different = true;
+                    differences++;
+                }
+            }
+        }
+        
+        assertTrue(different, "Two independent solutions should be different");
+        System.out.println("Number of different positions between two solutions: " + differences);
+    }
+}
