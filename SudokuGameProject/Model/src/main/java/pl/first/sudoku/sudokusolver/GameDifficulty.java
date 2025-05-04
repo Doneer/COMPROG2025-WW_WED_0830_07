1d78f28fb4b9dd33a2ccbc09e75f81e238c65a98
Daniyar Zhumatayev <253857@edu.p.lodz.pl>
2025-05-04 23:07:16 +0200

    Created a Difficulty Enum, Main Menu FXML and Main Menu Controller

---
+++ SudokuGameProject/Model/src/main/java/pl/first/sudoku/sudokusolver/GameDifficulty.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 Daniyar Zhumatayev, Kuzma Martysiuk
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package pl.first.sudoku.sudokusolver;
+
+/**
+ *
+ * @author zhuma
+ */
+
+public enum GameDifficulty {
+    EASY(20),
+    MEDIUM(40),
+    HARD(60);
+    
+    private final int cellsToRemove;
+    
+    GameDifficulty(int cellsToRemove) {
+        this.cellsToRemove = cellsToRemove;
+    }
+    
+    public int getCellsToRemove() {
+        return cellsToRemove;
+    }
+    
+    public SudokuBoard prepareBoard(SudokuBoard sourceBoard) {
+        SudokuBoard gameBoard = sourceBoard.clone();
+        int removed = 0;
+        int totalCells = 9 * 9;
+        
+        java.util.Random random = new java.util.Random();
+        
+        while (removed < cellsToRemove) {
+            int row = random.nextInt(9);
+            int col = random.nextInt(9);
+            
+            if (gameBoard.getValueAt(row, col) != 0) {
+                gameBoard.setValueAt(row, col, 0);
+                removed++;
+            }
+        }
+        
+        return gameBoard;
+    }
+}
---
+++ SudokuGameProject/View/src/main/java/pl/first/sudoku/view/MainMenuController.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 Daniyar Zhumatayev, Kuzma Martysiuk
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package pl.first.sudoku.view;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import pl.first.sudoku.sudokusolver.BacktrackingSudokuSolver;
+import pl.first.sudoku.sudokusolver.GameDifficulty;
+import pl.first.sudoku.sudokusolver.SudokuBoard;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author zhuma
+ */
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private ComboBox<GameDifficulty> difficultyComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        difficultyComboBox.setItems(FXCollections.observableArrayList(GameDifficulty.values()));
+        difficultyComboBox.getSelectionModel().selectFirst();
+    }
+    
+    @FXML
+    private void startGame() {
+        try {
+            SudokuBoard solvedBoard = new SudokuBoard(new BacktrackingSudokuSolver());
+            solvedBoard.solveGame();
+            
+            GameDifficulty selectedDifficulty = difficultyComboBox.getValue();
+            SudokuBoard gameBoard = selectedDifficulty.prepareBoard(solvedBoard);
+            
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pl/first/sudoku/view/SudokuBoardView.fxml"));
+            Parent root = loader.load();
+            
+            SudokuBoardController controller = loader.getController();
+            controller.setBoard(gameBoard);
+            
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) difficultyComboBox.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Sudoku Game - " + selectedDifficulty);
+            stage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
