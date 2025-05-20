4c65076331ae01a88f06fce272c487cd733e9aa3
Kuzma Martysiuk <253854@ude.p.lodz.pl>
2025-05-20 15:44:51 +0200

    Updated main menucontroller and mainmenuview

--- SudokuGameProject/View/src/main/java/pl/first/sudoku/view/MainMenuController.java
+++ SudokuGameProject/View/src/main/java/pl/first/sudoku/view/MainMenuController.java
@@ -25,12 +25,15 @@
 package pl.first.sudoku.view;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import pl.first.sudoku.sudokusolver.BacktrackingSudokuSolver;
 import pl.first.sudoku.sudokusolver.GameDifficulty;
@@ -38,6 +41,7 @@ import pl.first.sudoku.sudokusolver.SudokuBoard;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
@@ -49,11 +53,55 @@ public class MainMenuController implements Initializable {
 
     @FXML
     private ComboBox<GameDifficulty> difficultyComboBox;
-
+    
+    @FXML
+    private Label titleLabel;
+    
+    @FXML
+    private Label difficultyLabel;
+    
+    @FXML
+    private Button startGameButton;
+    
+    @FXML
+    private Button languageButton;
+    
+    private LanguageManager languageManager;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        difficultyComboBox.setItems(FXCollections.observableArrayList(GameDifficulty.values()));
+        languageManager = LanguageManager.getInstance();
+        
+        // Initialize difficulty options
+        ObservableList<GameDifficulty> difficulties = FXCollections.observableArrayList(
+                GameDifficulty.EASY,
+                GameDifficulty.MEDIUM, 
+                GameDifficulty.HARD);
+                
+        difficultyComboBox.setItems(difficulties);
         difficultyComboBox.getSelectionModel().selectFirst();
+        
+        // Set up language change listener
+        languageManager.localeProperty().addListener((observable, oldValue, newValue) -> {
+            updateTexts();
+        });
+        
+        updateTexts();
+    }
+    
+    private void updateTexts() {
+        ResourceBundle messages = languageManager.getMessagesBundle();
+        
+        titleLabel.setText(messages.getString("label.sudokuGame"));
+        difficultyLabel.setText(messages.getString("label.selectDifficulty"));
+        startGameButton.setText(messages.getString("button.startGame"));
+        languageButton.setText(messages.getString("button.changeLanguage"));
+        
+        // Update stage title
+        Stage stage = (Stage) titleLabel.getScene().getWindow();
+        if (stage != null) {
+            stage.setTitle(messages.getString("title.mainMenu"));
+        }
     }
     
     @FXML
@@ -67,6 +115,7 @@ public class MainMenuController implements Initializable {
             SudokuBoard gameBoard = selectedDifficulty.prepareBoard(solvedBoard);
             
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/pl/first/sudoku/view/SudokuBoardView.fxml"));
+            loader.setResources(languageManager.getMessagesBundle());
             Parent root = loader.load();
             
             SudokuBoardController controller = loader.getController();
@@ -75,11 +124,26 @@ public class MainMenuController implements Initializable {
             Scene scene = new Scene(root);
             Stage stage = (Stage) difficultyComboBox.getScene().getWindow();
             stage.setScene(scene);
-            stage.setTitle("Sudoku Game - " + selectedDifficulty);
+            
+            ResourceBundle messages = languageManager.getMessagesBundle();
+            stage.setTitle(messages.getString("title.game") + " - " + selectedDifficulty);
             stage.show();
             
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    @FXML
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private void changeLanguage() {
+        Locale currentLocale = languageManager.getLocale();
+        
+        // Toggle between English and Polish (or your native language)
+        if (currentLocale.equals(Locale.ENGLISH)) {
+            languageManager.setLocale(new Locale("pl"));
+        } else {
+            languageManager.setLocale(Locale.ENGLISH);
         }
     }
 }
